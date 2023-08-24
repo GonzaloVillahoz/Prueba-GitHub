48018f96db63dc412ca034467b07fa969be63812
gonzi <gonzi@GONZALO>
2023-08-23 22:37:12 -0300

    Agregar archivos de proyecto.

---
+++ Prueba GitHub/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba_GitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+
+            Console.ReadKey();
+        }
+    }
+}
